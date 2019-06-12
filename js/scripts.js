82c5833653e7f99b31767cf7b0fd7b973d368bf6
Bullkip <01sinister@mail.com>
2019-06-12 21:27:03 +0300

    fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-    $('.slider').slick({
-        dots:true
-    });
+    $('.card-content+.card-content').hide();
+    $('.card-tabs__link').first().addClass('active');
+
+    $('.card-tabs__link').on('click' , function (e) {
+        e.preventDefault();
+        $('.card-content').hide();
+        $('.card-tabs__link').removeClass('active');
+        $ ($(this).attr('href')).fadeIn();
+        $(this).addClass('active');
+    })
 });
